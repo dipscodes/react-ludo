cf10fddbc308c48c08d27d198e6f35c5124d554c
deep kumar khamaru <deepkumarkhamaru@gmail.com>
2023-08-18 10:59:02 +0530

    absoulute indexing hard boiled

--- src/Components/FinishBox.tsx
+++ src/Components/FinishBox.tsx
@@ -2,7 +2,7 @@ const FinishBox = () => {
   return (
     <div
       id="finish-box"
-      className="w-[120px] h-[120px] min-w-[120px] min-h-[120px] border-2 border-solid border-black"
+      className="w-[120px] h-[120px] min-w-[120px] min-h-[120px] border-2 border-solid border-black bg-white"
     ></div>
   );
 };
