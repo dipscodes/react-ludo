5855f2b06f6f98f49da183f56332c266bf20b5c2
deep kumar khamaru <deepkumarkhamaru@gmail.com>
2023-08-21 13:12:14 +0530

    changes according to classes

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -10,17 +10,17 @@ const Board = () => {
     >
       <div className="w-[600px] h-[240px] min-w-[600px] min-h-[240px] flex flex-row justify-normal">
         <HouseBox player={3} />
-        <ColorCorridor color="0" />
+        <ColorCorridor color={0} />
         <HouseBox player={0} />
       </div>
       <div className="w-[600px] h-[120px] min-w-[600px] min-h-[120px] flex flex-row justify-normal">
-        <ColorCorridor color="3" />
+        <ColorCorridor color={3} />
         <FinishBox />
-        <ColorCorridor color="1" />
+        <ColorCorridor color={1} />
       </div>
       <div className="w-[600px] h-[240px] min-w-[600px] min-h-[240px] flex flex-row justify-normal">
         <HouseBox player={2} />
-        <ColorCorridor color="2" />
+        <ColorCorridor color={2} />
         <HouseBox player={1} />
       </div>
     </div>
